04eee45125e1caa3fcd95670f992b73dd38ce1d1
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-29 16:33:15 +0530

    Add to clockify

--- index.js
+++ index.js
@@ -131,5 +131,13 @@ switchTimer(num ,str){
         timerType: str,
         alarmColor: {color: 'white'}});
 }
+clockify(){
+    let minutes = Math.floor(this.state.timer / 60);
+    let seconds = this.state.timer - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return minutes + ':' + seconds;
+}
+
 
 }
